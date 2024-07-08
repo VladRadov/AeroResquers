45c79d41f9915f1a0a6245bebb7a7f339b709a7d
VladRadov <picvladislav@yandex.ru>
2024-07-08 14:48:44 +0300

    fix: fixed override wave

--- Assets/AeroRescuers/Scripts/View/WaveView.cs
+++ Assets/AeroRescuers/Scripts/View/WaveView.cs
@@ -6,4 +6,11 @@ public class WaveView : EnemyView
 {
     public void SetActive(bool value)
         => gameObject.SetActive(value);
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        var wave = collision.gameObject.GetComponent<WaveView>();
+        if (wave != null)
+            transform.localPosition = transform.localPosition + new Vector3(0.1f, 0, 0);
+    }
 }
